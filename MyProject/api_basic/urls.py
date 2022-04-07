d796f7af278e1c768be094e12edf9c24efb67378
Home <you@example.com>
2022-04-07 22:49:23 +0700

    modified MyProject, add django-python

--- MyProject/api_basic/urls.py
+++ MyProject/api_basic/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import NewsP, Contact, Home
+from .views import NewsP, Contact, Home, Newsdate
 
 urlpatterns = [
     path('news', NewsP, name='news'),
     path('', Home, name='home'),
     path('Contact', Contact, name='contact'),
+    path('newsdate/<int:year>', Newsdate, name='newsdate'),
 ]
--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render, HttpResponse
 from .models import News
 # Create your views here.
@@ -17,6 +18,16 @@ def NewsP(request):
         "data":obj
     }
     return render(request, 'news.html', context)
+
+def Newsdate(request, year):
+    article_list=News.objects.filter(pub_date__year=year)
+
+    context = {
+        'year':year,
+        'article_list':article_list
+    }
+
+    return render(request, 'newsdate.html',context)
 
 def Contact(request):
     return render(request, 'contact.html')
