c2587b26bd08550a30b98717a55bd849ea97b438
QuocTrung76 <trung.truongttpro99@gmail.com>
2022-04-13 17:19:23 +0700

    apigetpost

---
+++ django-python/mysite/polls/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Question
+
+"""
+Serializer
+"""
+
+
+# class QuestionSerializers(serializers.Serializer):
+#     question_text = serializers.CharField(max_length=200)
+#     pub_date = serializers.DateTimeField()
+
+#     def create(self,validate_data):
+#         return Question.objects.create(validate_data)
+
+#     def update(self, instance, validate_data):
+#         instance.question_text=validate_data.get('question_text', instance.question_text)
+#         instance.pub_date=validate_data.get('pub_date', instance.pub_date)
+#         instance.save()
+#         return instance
+
+
+"""
+ModelSerializer
+"""
+class QuestionSerializers(serializers.ModelSerializer):
+    class Meta:
+        model=Question
+        fields=['pub_date','question_text']
--- django-python/mysite/polls/views.py
+++ django-python/mysite/polls/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.views import generic
+from rest_framework.parsers import JSONParser
 from .models import Question, Choice
 from django.utils import timezone
+from .serializers import QuestionSerializers
 
+"""
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
+        #Return the last five published questions.
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
@@ -41,4 +44,22 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    """
 # Create your views here.
+
+def question_list(request):
+
+    if request.method=='GET':
+        template_name = 'polls/question_text.html'
+        question_text=Question.objects.all()
+        serializer=QuestionSerializers(question_text, many=True)
+        return JsonResponse(serializer.data,safe=False)
+    elif request.method=='POST':
+        template_name = 'polls/question_text.html'
+        data=JSONParser().parse(request)
+        serializer=QuestionSerializers(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data,status=201)
+        return JsonResponse(serializer.errors, status=400)
