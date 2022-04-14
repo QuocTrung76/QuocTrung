1d9e3ffdcbdaadead15132493b710c2c4a016640
Trung <you@example.com>
2022-04-14 17:11:40 +0700

    getpost2

--- django-python/mysite/polls/urls.py
+++ django-python/mysite/polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from . import apiviews
 app_name='polls'
 urlpatterns = [
         #path('', views.IndexView.as_view(), name='index'),
@@ -11,4 +12,5 @@ urlpatterns = [
         path('<int:pk>/', views.question_detail, name='detail'),
         path('<int:question_id>/vote/', views.vote, name='vote'),
         path('detail/<int:question_id>/', views.DetailView.as_view(), name='detail'),
+        path('question/', apiviews.question_view, name='question_view'),
 ]
