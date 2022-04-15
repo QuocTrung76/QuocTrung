ecc9147a3343d69afe85d5ca8d6273e981382364
Trung <you@example.com>
2022-04-15 11:44:24 +0700

    modified nothing

--- django-python/mysite/mysite/urls.py
+++ django-python/mysite/mysite/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('api/polls/',include('polls.urls'))
 
 ]
